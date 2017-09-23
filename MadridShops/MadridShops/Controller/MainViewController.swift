1f223850547671551d17efb61ea8d22996d9cef2
Fabio Gomez <gomez.fabio@gmail.com>
2017-09-24 01:52:59 +0200

    Added background image, created a new class for the main view controller

---
+++ MadridShops/MadridShops/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  MadridShops
+//
+//  Created by Fabio Gomez on 24/9/17.
+//  Copyright © 2017 Fabio Gomez. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Aqui el pod del ruso
+    }
+
+
+}
